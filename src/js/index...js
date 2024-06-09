3f399065b1e11df005b8d84e88b3a5cdfc963a24
koso <108082170+kosoneto@users.noreply.github.com>
2024-06-09 18:12:38 -0300

    Add files via upload

---
+++ src/js/index...js
@@ -0,0 +1,30 @@
+// comentar CSSTranslate+;
+
+/* Objetivos
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
+cartao da lista
+  - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+  - passo 3 - fazer aparecer o próximo cartão da lista 
+  - passo 4 - buscar o cartão que esta selecionado e esconder
+
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
+anterior da lista
+  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+  - passo 3 - fazer aparecer o cartão anterior da lista
+  - passo 4 - buscar o cartão que esta selecionado e esconder
+  */
+
+
+//   OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
+//   cartao da lista
+//     - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+// const btnAvancar = document.getElementById("btn-avancar"); //    pegar o id do botao avancar 
+// // - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+// btnAvancar.addEventListener("click", function (){
+// console.log("cliquei")
+// // - passo 3 - fazer aparecer o próximo cartão da lista
+// const cartoes = document.querySelectorAll(".cartao")// busca todas a s listas ,  #busca classe . buscaelemento pelo id 
+
+// })
